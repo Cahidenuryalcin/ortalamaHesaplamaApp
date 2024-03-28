62aeb4ea6a22f72006c7df7aacee866c37944fb6
Cahidenuryalcin <cnuryal@gmail.com>
2024-03-28 15:54:00 +0300

    28.03.24

---
+++ lib/widgets/ortalama_hesapla_page.dart
@@ -0,0 +1,35 @@
+
+import 'package:dinamik_ortalama_hesaplama_app/app_constanst.dart';
+import 'package:flutter/material.dart';
+
+class OrtalamaHesaplaPage extends StatefulWidget {
+  const OrtalamaHesaplaPage({super.key});
+
+  @override
+  State<OrtalamaHesaplaPage> createState() => _OrtalamaHesaplaPageState();
+}
+
+class _OrtalamaHesaplaPageState extends State<OrtalamaHesaplaPage> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Center(
+          child: Text(
+            Sabitler.baslikText,
+            style: Sabitler.baslikStyle,
+          ),
+        ),
+      ),
+      body: Column(
+        
+      ),
+    );
+  }
+
+  
+}
+
